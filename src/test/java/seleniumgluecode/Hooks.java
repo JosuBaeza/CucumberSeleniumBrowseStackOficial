4e30f0ca2e91d4bfdb640dff728d0d76ddd0044c
Josue Baeza <josue.baeza@tcs.com>
2021-07-22 14:39:55 -0400

    cambios

--- src/test/java/seleniumgluecode/Hooks.java
+++ src/test/java/seleniumgluecode/Hooks.java
@@ -62,11 +62,11 @@ public class Hooks {
         public void setUp(){
             System.setProperty("webdriver.chrome.driver","./src/test/resources/chromedriver/chromedriver.exe");
 
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--headless");
+            //ChromeOptions options = new ChromeOptions();
+            //options.addArguments("--headless");
 
-            webDriver  = new ChromeDriver(options);
-
+           // webDriver  = new ChromeDriver(options);
+            webDriver  = new ChromeDriver();
             webDriver.get("http://live.demoguru99.com/index.php/");
             webDriver.manage().window().maximize();
         }
